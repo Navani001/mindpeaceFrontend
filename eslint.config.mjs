8a0376868f0c06c0bbe35f4e88e179a1ac283886
navani_hk <navaneetha2006krishnan@gmail.com>
2026-04-01 23:49:05 +0530

    deployee

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    rule:{
+    rules: {
          "@typescript-eslint/no-explicit-any": "off",
     }
   }
